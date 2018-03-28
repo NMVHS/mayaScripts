14f839775a13848481a465e23338c666468a3029
shawn <shawn@newhollandcreative.com>
2018-03-28 11:24:15 +1100

    add set manager

---
+++ SetManager.py
@@ -0,0 +1,50 @@
+#set manager
+#shawn - March 2018
+import maya.cmds as cm
+
+def main():
+    #----ui---------------
+    newWin = "SetManager"
+    if cm.window(newWin,exists=True):
+        cm.deleteUI(newWin,window=True)
+    
+    cm.window(newWin,title="Set Manager",width=200,height=200)
+    baseLayout= cm.columnLayout(rowSpacing=2)
+    
+    cm.button("Create new",height=50,width=200,command = newSet)
+    cm.button("Add to selected",height=50,width=200,command = addToSet)
+    cm.button("Select set members",height=50,width=200,command = selectSet)
+    
+    cm.showWindow(newWin)
+
+def selectSet(*args):
+    sel = cm.ls(sl=True)
+    cm.select(clear=True)
+    for each in sel:
+        if cm.objectType(each) == "objectSet":
+            cm.select(each,add=True)
+        else:
+            belongSets = cm.listSets(object=each)
+            cm.select(belongSets)
+                
+def newSet(*args):
+    sel = cm.ls(sl=True,flatten=True)
+    result = cm.promptDialog(title='Create New Name',message='Set Name:',button=['OK','Cancel'],
+                                defaultButton='OK',cancelButton='Cancel',dismissString='Cancel')
+
+    if result == 'OK':
+    	setName = cm.promptDialog(query=True,text=True)
+    	cm.sets(name=setName,empty=True)
+    	cm.sets(sel,add=setName)
+    	
+def addToSet(*args):
+    sel = cm.ls(sl=True,flatten=True)
+    if cm.objectType(sel[-1]) == "objectSet":
+        cm.sets(sel[:-1],add=sel[-1])
+    else:
+        belongSets = cm.listSets(object=sel[-1])
+        for eachSet in belongSets:
+            cm.sets(sel[:-1],add=eachSet)
+
+if __name__ == "__main__":
+    main()
