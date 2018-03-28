f364ce350a6797cda984a46ea9946ccfe0a0c5b6
shawn <shawn@newhollandcreative.com>
2018-03-28 11:39:00 +1100

    add select set name

--- SetManager.py
+++ SetManager.py
@@ -11,13 +11,14 @@ def main():
     cm.window(newWin,title="Set Manager",width=200,height=200)
     baseLayout= cm.columnLayout(rowSpacing=2)
     
-    cm.button("Create new",height=50,width=200,command = newSet)
-    cm.button("Add to selected",height=50,width=200,command = addToSet)
-    cm.button("Select set members",height=50,width=200,command = selectSet)
+    cm.button("Create New Set",height=50,width=200,command = newSet)
+    cm.button("Add to Selected",height=50,width=200,command = addToSet)
+    cm.button("Select Set Name Only",height=50,width=200,command = selectSet)
+    cm.button("Select Set Members",height=50,width=200,command = selectSetMembers)
     
     cm.showWindow(newWin)
 
-def selectSet(*args):
+def selectSetMembers(*args):
     sel = cm.ls(sl=True)
     cm.select(clear=True)
     for each in sel:
@@ -45,6 +46,13 @@ def addToSet(*args):
         belongSets = cm.listSets(object=sel[-1])
         for eachSet in belongSets:
             cm.sets(sel[:-1],add=eachSet)
+
+def selectSet(*args):
+    sel = cm.ls(sl=True,flatten=True)
+    for each in sel:
+        if cm.objectType(each) != "objectSet":
+            belongSets = cm.listSets(object=each)
+            cm.select(belongSets,noExpand=True)
 
 if __name__ == "__main__":
     main()
